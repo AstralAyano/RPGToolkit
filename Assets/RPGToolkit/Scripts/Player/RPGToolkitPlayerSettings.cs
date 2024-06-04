4f486c46ccc0ab26c1799cbd9ae22ec91de3e234
Bernard Ng <bernardngboonhao@gmail.com>
2024-06-04 18:10:12 +0800

    Link `RPGToolkitManager` to `RPGToolkitPlayer` prefab

---
+++ Assets/RPGToolkit/Scripts/Player/RPGToolkitPlayerSettings.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPGToolkit
+{
+    public class RPGToolkitPlayerSettings : MonoBehaviour
+    {
+        [Header("Resources")]
+        public RPGToolkitManager RPGToolkitAsset;
+        private PlayerController playerController;
+
+        private void Awake()
+        {
+            playerController = GetComponent<PlayerController>();
+
+            playerController.hasInventory = RPGToolkitAsset.hasInventory;
+            playerController.hasQuestBook = RPGToolkitAsset.hasQuestBook;
+        }
+    }
+}
